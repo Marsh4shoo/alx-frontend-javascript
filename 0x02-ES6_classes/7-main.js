05baccb11f090a3d1c708e4b7ed96626f2f8876b
Marsh4shoo <marshal.wjnr@gmail.com>
2024-06-27 20:39:45 +0000

    Updated

---
+++ 0x02-ES6_classes/7-main.js
@@ -0,0 +1,6 @@
+// 7-main.js
+import Airport from './7-airport.js';
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);             // Should print: Airport { _name: 'San Francisco Airport', _code: 'SFO' }
+console.log(airportSF.toString());  // Should print: [object SFO]
