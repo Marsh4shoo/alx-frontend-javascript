3b47c06870172fa85e58a19268513492c0582a34
Marsh4shoo <marshal.wjnr@gmail.com>
2024-06-27 20:10:11 +0000

    Updated

---
+++ 0x02-ES6_classes/3-main.js
@@ -0,0 +1,10 @@
+// ./3-currency.js
+export class Currency {
+    constructor(symbol, name) {
+        // ... your class implementation
+    }
+
+    displayFullCurrency() {
+        // ... your method implementation
+    }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+0x02. ES6 classes By Marshal Zvinoira
